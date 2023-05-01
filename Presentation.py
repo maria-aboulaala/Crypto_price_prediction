8729ac734498fbaaef12c782760f84ae6b5ab798
Maria Aboulaala <mariaaboulaala28@gmail.com>
2023-05-01 07:32:36 +0100

    day1

--- Presentation.py
+++ Presentation.py
@@ -161,17 +161,46 @@ for p in range(p_max+1):
                 
 
 
-#st.write(best_order)
 
 #st.write('Best ARIMA{} model - AIC:{}'.format(best_order, best_aic))
-#st.write("best arima model", best_order)
+st.write("Best arima model", best_order)
+st.write("AIC:", best_aic)
 # Ajuster le modèle ARIMA(p,q) avec les données d'entraînement
-#model = ARIMA(train, order=best_order)
-#results = model.fit()
+model = sm.tsa.arima.ARIMA(train, order=best_order)
+results = model.fit()
 
 # Afficher un résumé des résultats du modèle ajusté
+st.write(results.summary())
+#-------
+residuals = pd.DataFrame(results.resid, columns=['Residuals'])
+fig, ax = plt.subplots(figsize=(10, 5))
+residuals.plot(ax=ax)
+ax.set(title='Résidus du modèle ARIMA(2,1,4)', ylabel='Valeurs résiduelles')
+z=plt.show()
+st.pyplot(z)
+# Tracer la fonction d'autocorrélation des résidus
+fig, ax = plt.subplots(figsize=(10, 5))
+plot_acf(residuals, ax=ax, lags=20)
+w= plt.show()
+st.pyplot(w)
 
-#st.write(results.summary())
+#------
+n_periods = len(test)
+forecast = results.forecast(steps=n_periods)
+st.write(forecast)
 
+mse = ((forecast - test) ** 2).mean()
+st.write("Mean Squared Error (MSE):", mse)
 
+fig, ax = plt.subplots(figsize=(10, 5))
+plt.plot(test.index, test.values, label='Données de test')
+plt.plot(test.index, forecast, label='Prévisions')
+ax.set(title='Prévisions avec le modèle arima')
+st.pyplot(fig)
+
+#[theme]
+#primaryColor="#a4d0a4"
+#backgroundColor="#fff8d6"
+#secondaryBackgroundColor="#f7e1ae"
+#textColor="#41644a"
 
